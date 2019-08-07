84917d2ce8f0f9a6e305a1da7724c8bde779340d
zyy <zhangyiyanghz@gmail.com>
2019-08-07 18:21:30 +0800

    09，查看数据库，获取数据表，根据key查值

--- bolt/main.go
+++ bolt/main.go
@@ -11,8 +11,12 @@ var (
 	err error
 )
 
+const bucketname = "bucket01"
+
 func main() {
 	usageOpen()
+	usageUpdateDB()
+	usageViewDB()
 }
 
 func usageOpen() {
@@ -29,13 +33,38 @@ func usageUpdateDB() {
 			err    error
 			bucket *bolt.Bucket
 		)
-		if bucket, err = tx.CreateBucket([]byte("bucket01")); err != nil {
+
+		if bucket = tx.Bucket([]byte(bucketname)); bucket != nil {
+			return nil
+		}
+
+		if bucket, err = tx.CreateBucket([]byte(bucketname)); err != nil {
 			return fmt.Errorf("创建表失败%s", err)
 		}
 
 		if err = bucket.Put([]byte("l"), []byte("给你 100 BTC")); err != nil {
 			return fmt.Errorf("写入数据失败%s", err)
 		}
+
+		return nil
+	}); err != nil {
+		log.Panic(err)
+	}
+}
+
+func usageViewDB() {
+
+	if err = db.View(func(tx *bolt.Tx) error {
+		var (
+			bucket *bolt.Bucket
+			result []byte
+		)
+		if bucket = tx.Bucket([]byte(bucketname)); bucket == nil {
+			return fmt.Errorf("获取表对象失败")
+		}
+
+		result = bucket.Get([]byte("l"))
+		fmt.Println(string(result))
 
 		return nil
 	}); err != nil {
