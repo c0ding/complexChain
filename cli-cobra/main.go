c99034700128641c94d70cd8315646786c57847b
张逸阳 <zhangyiyanghz@gmail.com>
2019-09-29 17:21:47 +0800

    fix

--- cli-cobra/main.go
+++ cli-cobra/main.go
@@ -1,22 +1,21 @@
-
 package main
 
 import (
+	BLC "github.com/c0ding/complexChain/base-prototype/blockchain"
 	"github.com/c0ding/complexChain/cli-cobra/cmd"
-	//BLC "github.com/c0ding/complexChain/base-prototype/blockchain"
 )
 
 func main() {
 
-	//var (
-	//	blockchain *BLC.Blockchain
-	//)
-	//
-	//if blockchain == nil {
-	//	blockchain = BLC.NewBlockchainWithGenesisBlock()
-	//}
-	//
-	//defer blockchain.DB.Close()
+	var (
+		blockchain *BLC.Blockchain
+	)
+
+	if blockchain == nil {
+		blockchain = BLC.NewBlockchainWithGenesisBlock()
+	}
+
+	defer blockchain.DB.Close()
 	cmd.Execute()
 
 }
